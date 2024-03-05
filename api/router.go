bdb31877a60055ff5bf4f7ef53f576a3893a3106
水告木南 <Haonan_Fang@outlook.com>
2024-03-05 17:14:58 +0800

    增加路由接口配置

---
+++ api/router.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	return router
+}
