a82413973dd43db06d7811f3be8f6b0f4d949237
水告木南 <Haonan_Fang@outlook.com>
2024-03-05 16:41:50 +0800

    增加依赖,更新·房屋和用户的结构

--- pkg/model/house.go
+++ pkg/model/house.go
@@ -1,1 +1,15 @@
 package model
+
+import (
+	"gorm.io/gorm"
+)
+
+type House struct {
+	gorm.Model
+	Owner       string  `json:"name"`     // 房屋所有者
+	OwnerID     uint    `gorm:"not null"` // 房屋所有者ID
+	Title       string  `gorm:"not null"` // 房屋标题
+	Description string  // 房屋描述
+	Price       float64 `gorm:"not null"` // 房屋价格
+	Address     string  `gorm:"not null"` // 房屋地址
+}
