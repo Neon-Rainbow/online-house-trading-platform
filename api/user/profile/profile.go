7167de70516644caf2be8ac854a46d41dd32c61b
水告木南 <Haonan_Fang@outlook.com>
2024-03-06 16:27:57 +0800

    增加了用户个人信息的api

---
+++ api/user/profile/profile.go
@@ -0,0 +1,24 @@
+// Package profile 提供了用户个人信息,例如名字,年龄等的API
+package profile
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// ProfileGet 用于处理用户的个人信息界面的GET请求, 该界面需要登录后才能访问, 未登录用户将被重定向到登录界面,
+// 该界面获取用户的个人信息, 包括用户名, 邮箱, 电话号码等
+func ProfileGet(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "/user/profile",
+		"method":  "GET",
+	})
+}
+
+// ProfilePost 用于处理用户的个人信息界面的POST请求, 该界面需要登录后才能访问, 未登录用户将被重定向到登录界面,
+// 发送该post请求后应该需要被redirect到/user/profile/update界面,该部分应该是前端来完成
+func ProfilePost(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "/user/profile",
+		"method":  "POST",
+	})
+}
---
+++ api/user/profile/update.go
@@ -0,0 +1,21 @@
+package profile
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// ProfileUpdateGet 用于获取更新用户信息的界面,需要登陆后访问
+func ProfileUpdateGet(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "/user/profile/update",
+		"method":  "GET",
+	})
+}
+
+// ProfileUpdatePost 用于处理更新用户信息的请求,需要登陆后访问
+func ProfileUpdatePost(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "/user/profile/update",
+		"method":  "POST",
+	})
+}
