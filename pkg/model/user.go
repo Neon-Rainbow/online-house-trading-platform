3ad0cc9f8c5b026cf091da42871a963933b0ca61
水告木南 <Haonan_Fang@outlook.com>
2024-03-07 01:15:38 +0800

    修改user数据结构,用户名,密码,邮箱不能为空,用户名和邮箱不能重复

--- pkg/model/user.go
+++ pkg/model/user.go
@@ -7,8 +7,8 @@ import (
 // User 用户模型
 type User struct {
 	gorm.Model
-	Username string `json:"username", gorm:"uniqueIndex;not null"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
+	Username string `json:"username" gorm:"uniqueIndex;not null;type:varchar(255)"`
+	Password string `json:"password" gorm:"not null;type:varchar(255)"`
+	Email    string `json:"email" gorm:"uniqueIndex;not null;type:varchar(255)"`
 	Role     string `json:"role"`
 }
