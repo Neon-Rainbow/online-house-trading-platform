8d6adf0130a712104d6391f843bfb5daefed30c0
水告木南 <Haonan_Fang@outlook.com>
2024-03-07 01:15:54 +0800

    完善/auth/register接口

--- api/auth/register.go
+++ api/auth/register.go
@@ -4,8 +4,10 @@ import (
 	"log"
 	"net/http"
 	"online-house-trading-platform/pkg/model"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 // RegisterGet 用于处理用户的注册界面的GET请求
@@ -18,16 +20,43 @@ func RegisterGet(c *gin.Context) {
 
 // RegisterPost 用于处理用户的注册界面的POST请求
 func RegisterPost(c *gin.Context) {
+	db, exists := c.MustGet("db").(*gorm.DB)
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "无法获取数据库连接"})
+		return
+	}
+
 	var user model.User
 	err := c.ShouldBind(&user)
 	if err != nil {
-		log.Printf("error: %v", err)
+		log.Printf("Error binding data: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "请求参数错误"})
+		return
 	}
+	if user.Username == "" || user.Password == "" || user.Email == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "用户名、密码和邮箱不能为空",
+		})
+		return
+	}
+
+	err = db.Create(&user).Error
+	if err != nil {
+		if strings.Contains(err.Error(), "1062") {
+			if strings.Contains(err.Error(), "idx_users_username") {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "用户名已存在"})
+				return
+			} else if strings.Contains(err.Error(), "idx_users_email") {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "邮箱已被注册"})
+				return
+			}
+		}
+		log.Printf("Error creating user: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "注册失败"})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"message":  "/auth/register",
-		"method":   "POST",
-		"username": user.Username,
-		"email":    user.Email,
+		"message": "注册成功",
 	})
-	//TODO: 这里需要实现保存用户信息到数据库
 }
