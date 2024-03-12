47032ac6a14dbffc332755c8c935ee462f87071a
水告木南 <Haonan_Fang@outlook.com>
2024-03-12 16:00:15 +0800

    实现了/houses/appointment接口的post请求,即实现了预约请求

--- api/houses/appointment.go
+++ api/houses/appointment.go
@@ -2,14 +2,41 @@ package houses
 
 import (
 	"net/http"
+	"online-house-trading-platform/pkg/model"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 // HousesAppointmentPost 用于实现用户预约房屋
 func HousesAppointmentPost(c *gin.Context) {
+	db, exists := c.MustGet("db").(*gorm.DB)
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "无法获取数据库连接",
+		})
+		return
+	}
+
+	var reserve model.Reserve
+	err := c.ShouldBind(&reserve)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "请求参数错误",
+		})
+		return
+	}
+
+	err = db.Create(&reserve).Error
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "预约失败",
+		})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"message": "/houses/appointment",
-		"method":  "POST",
+		"message": "预约成功",
+		"url":     "/houses/appointment",
 	})
 }
