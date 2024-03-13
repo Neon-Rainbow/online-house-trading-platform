8c1b248d71adc72157513c4dfecfc44b374ddd57
水告木南 <Haonan_Fang@outlook.com>
2024-03-14 01:45:49 +0800

    增加了对主页面和404页面的html渲染

---
+++ api/root/setup.go
@@ -0,0 +1,10 @@
+package root
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func SetUpRootAPI(router *gin.Engine, db *gorm.DB) {
+	router.GET("/", HomePageGet)
+}
--- api/router.go
+++ api/router.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"net/http"
+	"online-house-trading-platform/api/root"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 
@@ -19,10 +22,11 @@ import (
 // 5. 设置/houses路由,用于处理房屋信息请求
 // 该函数使用了数据库连接,用于处理用户的信息请求
 func SetupRouter(db *gorm.DB) *gin.Engine {
+
 	router := gin.Default()
 
 	//加载静态文件
-	router.Static("/web/static", "static")
+	router.Static("./web/static", "static")
 
 	//加载模板文件
 	router.LoadHTMLGlob("./web/templates/**/*")
@@ -35,6 +39,14 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
 
 	//设置路由,地址为/houses
 	houses.SetUpHousesAPI(router, db)
+
+	//设置路由,地址为/
+	root.SetUpRootAPI(router, db)
+
+	//404界面
+	router.NoRoute(func(c *gin.Context) {
+		c.HTML(http.StatusNotFound, "404.html", nil)
+	})
 
 	return router
 }
