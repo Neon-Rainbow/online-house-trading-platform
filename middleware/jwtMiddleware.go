5c9df54f8a724e535b7b787cacdb8a5c2a519f86
水告木南 <Haonan_Fang@outlook.com>
2024-03-15 02:01:16 +0800

    Add token format validation and logging in JWT middleware

--- middleware/jwtMiddleware.go
+++ middleware/jwtMiddleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
 	"online-house-trading-platform/pkg/jwt"
 	"strings"
@@ -21,7 +22,25 @@ func JWTAuthMiddleware() func(c *gin.Context) {
 
 		parts := strings.SplitN(authHeader, " ", 2)
 
-		mc, err := jwt.ParseToken(parts[0])
+		var tkn string
+		// 如果请求头部的token格式不正确，则返回错误信息
+		// 正确的格式为：Bearer token 或者 token
+		if len(parts) == 1 {
+			tkn = parts[0]
+		} else if len(parts) == 2 && parts[0] == "Bearer" {
+			tkn = parts[1]
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "请求携带的token格式错误，无权限访问",
+			})
+			c.Abort()
+			return
+		}
+
+		log.Printf("authHeader: %v", authHeader)
+		log.Printf("parts: %v", parts)
+
+		mc, err := jwt.ParseToken(tkn)
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{
 				"error": "无效的token",
