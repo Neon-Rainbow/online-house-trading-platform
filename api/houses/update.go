678de862979f19162e1409be6898575197995cf7
水告木南 <Haonan_Fang@outlook.com>
2024-03-14 19:41:05 +0800

    重定向了/houses/update接口,现在该接口指向了/user/release

--- api/houses/update.go
+++ api/houses/update.go
@@ -6,18 +6,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// UpdateGet 用于处理用户的添加房屋界面的GET请求
+// UpdateGet 用于处理用户的更新房屋界面的GET请求
 func UpdateGet(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "/houses/update",
-		"method":  "GET",
-	})
+	//c.JSON(http.StatusOK, gin.H{
+	//	"message": "/houses/update",
+	//	"method":  "GET",
+	//})
+	c.Redirect(http.StatusMovedPermanently, "/user/release")
 }
 
-// UpdatePost 用于处理用户的添加房屋界面的POST请求
+// UpdatePost 用于处理用户的更新房屋界面的POST请求
 func UpdatePost(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "/houses/update",
 		"method":  "POST",
 	})
+	c.Redirect(http.StatusMovedPermanently, "/user/release")
 }
