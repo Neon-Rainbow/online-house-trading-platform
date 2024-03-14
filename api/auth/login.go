92a6d5110faf5cab95b98e35d0c81a980a7b9f91
水告木南 <Haonan_Fang@outlook.com>
2024-03-14 19:40:28 +0800

    登陆后给前端发送的数据中带上了token

--- api/auth/login.go
+++ api/auth/login.go
@@ -1,11 +1,14 @@
 package auth
 
 import (
+	"errors"
 	"log"
 	"net/http"
+	"online-house-trading-platform/pkg/jwt"
 	"online-house-trading-platform/pkg/model"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 // LoginGet 用于处理用户的登录界面的GET请求
@@ -21,6 +24,12 @@ func LoginGet(c *gin.Context) {
 // LoginPost 用于处理用户的登录界面的POST请求
 // 返回状态码200和登录成功的信息
 func LoginPost(c *gin.Context) {
+	db, exists := c.MustGet("db").(*gorm.DB)
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "无法获取数据库连接"})
+		return
+	}
+
 	var user model.User
 	err := c.ShouldBind(&user)
 	if err != nil {
@@ -31,10 +40,39 @@ func LoginPost(c *gin.Context) {
 		return
 	}
 
+	var dbUser model.User
+	result := db.Where("username = ?", user.Username).First(&dbUser)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "找不到用户",
+			})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "数据库查询出错",
+			})
+		}
+		return
+	}
+	if encryptPassword(user.Password) != dbUser.Password {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "密码错误",
+		})
+		return
+	}
+	token, err := jwt.GenerateToken(user.Username, user.ID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "无法生成token",
+		})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"message":  "/auth/login",
-		"method":   "POST",
-		"username": user.Username,
-		"password": user.Password,
+		"message": "登录成功",
+		"data": gin.H{
+			"token": token,
+		},
 	})
+
 }
