c8bb07d6d16bb0ea0e69013b2faf7319975a255f
水告木南 <Haonan_Fang@outlook.com>
2024-04-15 19:50:51 +0800

    feat:增加了发布房屋信息的接口

--- api/user/release.go
+++ api/user/release.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"online-house-trading-platform/pkg/model"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 // uploadFile 用于处理上传文件的请求
@@ -60,11 +62,38 @@ func ReleaseGet(c *gin.Context) {
 
 // ReleasePost 用于处理用户发布信息界面的POST请求,用于发布新的房源
 func ReleasePost(c *gin.Context) {
-	uploadFile(c)
+	// 从上下文中获取数据库连接
+	db, exist := c.MustGet("db").(*gorm.DB)
+	if !exist {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "无法获取数据库连接",
+		})
+		return
+	}
+
+	uploadFile(c) // 上传文件
+
+	// 绑定数据
+	var house model.House
+	err := c.ShouldBind(&house)
+	if err != nil {
+		log.Printf("绑定数据失败,错误原因: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "绑定数据失败",
+		})
+		return
+	}
+
+	err := db.Create(&house).Error
+	if err != nil {
+		log.Printf("创建房屋失败,错误原因: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "创建房屋失败",
+		})
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"url":     "/user/release",
-		"method":  "POST",
 		"message": "发布成功",
 	})
 }
