7196e8bcbfaaca74b51903464b623defed825a76
水告木南 <Haonan_Fang@outlook.com>
2024-04-16 13:54:37 +0800

    feat<dao>:新增创建用户和检查用户名和邮箱是否存在的函数

--- internal/dao/user.go
+++ internal/dao/user.go
@@ -12,3 +12,24 @@ func GetUserByUsername(db *gorm.DB, username string) (*model.User, error) {
 	result := db.Where("username = ?", username).First(&user)
 	return &user, result.Error
 }
+
+// CreateUser 用于创建用户
+func CreateUser(db *gorm.DB, user *model.User) error {
+	return db.Create(user).Error
+}
+
+// CheckUserExists 用于检查用户名和邮箱是否已存在,返回两个bool值,第一个bool值表示用户名是否存在,第二个bool值表示邮箱是否存在
+func CheckUserExists(db *gorm.DB, username, email string) (bool, bool, error) {
+	var count int64
+	if err := db.Model(&model.User{}).Where("username = ?", username).Count(&count).Error; err != nil {
+		return false, false, err
+	}
+	usernameExists := count > 0
+
+	if err := db.Model(&model.User{}).Where("email = ?", email).Count(&count).Error; err != nil {
+		return false, false, err
+	}
+	emailExists := count > 0
+
+	return usernameExists, emailExists, nil
+}
