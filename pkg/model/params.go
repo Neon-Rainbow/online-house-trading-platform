cccf4ec168b7db4ab2ddd12e893194ab27d28cf7
水告木南 <Haonan_Fang@outlook.com>
2024-05-09 14:15:51 +0800

    feat:更新上传文件的接口,现在发布房屋信息的接口可以正常工作了

--- pkg/model/params.go
+++ pkg/model/params.go
@@ -28,13 +28,13 @@ type RegisterRequest struct {
 
 // HouseRequest 是从前端接收房源数据的结构体。
 type HouseRequest struct {
-	Owner       string                  `json:"owner"`       // 房屋所有者名称
-	OwnerID     uint                    `json:"owner_id"`    // 房屋所有者ID
-	Title       string                  `json:"title"`       // 房屋标题
-	Description string                  `json:"description"` // 房屋描述
-	Price       float64                 `json:"price"`       // 房屋价格
-	Address     string                  `json:"address"`     // 房屋地址
-	Images      []*multipart.FileHeader `json:"images"`      // 房屋相关的图片文件列表
+	Owner       string                  `json:"owner" form:"owner"`             // 房屋所有者名称
+	OwnerID     uint                    `json:"owner_id" form:"owner_id"`       // 房屋所有者ID
+	Title       string                  `json:"title" form:"title"`             // 房屋标题
+	Description string                  `json:"description" form:"description"` // 房屋描述
+	Price       float64                 `json:"price" form:"prices"`            // 房屋价格
+	Address     string                  `json:"address" form:"address"`         // 房屋地址
+	Images      []*multipart.FileHeader `json:"images" form:"images"`           // 房屋相关的图片文件列表
 }
 
 // Error 用于处理错误信息
